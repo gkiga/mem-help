9c745d75048a60b64dae9b6048b33cc23ae4c3d6
Sachita K <sachitatakhanal101@email.com>
2020-09-29 00:01:35 -0500

    added image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #root route
-  root to: redirect('/home', status:302)
+  #root to: redirect('/home', status:302)
 
   #homepage
   get 'home', to: 'static_pages#home', as: 'home'
@@ -12,13 +12,10 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-<<<<<<< HEAD
 
   #Profile Page
   get 'profile', to: 'static_pages#profile', as: 'profile'
-=======
   get 'users', to: 'users#index', as: 'users'
->>>>>>> origin/MH-1
   
     
 
