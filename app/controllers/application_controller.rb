26b42d8372aad1045f44d99ff2f5acc667e4889b
Chris Moore <csunidev@gmail.com>
2020-10-12 12:46:20 -0500

    fixed sanitation issue with devise and worked on inventory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
           
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-            devise_parameter_sanitizer.permit(:account_update, keys: [:avatar]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:contact_info]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:major]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:interests]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:bio]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:first_name]) 
+            devise_parameter_sanitizer.permit(:account_update, keys: [:last_name]) 
          end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -37,7 +37,8 @@ class Request < ApplicationRecord
     #before_destroy -> { users.each {|u| u.group = nil; u.save}}
     #def init
      # self.completedFlag  ||= false          #will set the default value only if it's nil
-      
+     
+  
   end  
 
 
