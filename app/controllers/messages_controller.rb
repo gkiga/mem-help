ad44f68b21e8ad00f43558b848a6fe1809a50771
Sohail Khalid <skhalid@memphis.edu>
2020-10-17 13:46:47 -0500

    procssing message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,98 @@
+class MessagesController < ApplicationController
+    before_action :authenticate_user!
+ 
+    def index
+    
+         messages = Message.all
+         respond_to do |format|
+             format.html { render :index, locals: { messages: messages } }
+         end
+     end
+
+    def show
+        #@user = User.find(params[:id])
+        message = Message.find(params[:id])
+        respond_to do |format|
+            format.html { render :show, locals: { message: message } }
+        end
+    end
+
+     def new
+        message = Message.new(params[:message])
+         respond_to do |format|
+             format.html { render :new, locals: { message: message } }
+         end
+     end
+    
+    def create
+         # new object from params
+         #@user = User.all
+         message = Message.new(params.require(:message).permit(:body, :title, :recipient_name,:sender_name, :recipient_id))
+         # respond_to block
+         message.user_id = current_user.id
+         message.sender_name = [current_user.first_name, current_user.last_name].join(' ')
+        
+         respond_to do |format|
+             format.html do
+                 if message.save
+                    #@user = User.find(params[:recipient])
+                   # request.recipient_name = [@user.first_name, @user.last_name].join(' ')
+                     # success message
+                     flash[:success] = "Message saved successfully"
+                     # redirect to index
+                     redirect_to message_url
+                 else
+                     # error message
+                     flash.now[:error] = "Error: Message could not be saved"
+                     # render new
+                     render :new, locals: { message: message }
+                 end
+             end
+         end
+       
+     end
+
+    def edit
+        message = Message.find(params[:id])
+        
+        respond_to do |format|
+            format.html { render :edit, locals: { message: message } }
+        end
+    end
+
+    def update
+        # load existing object again from URL param
+        message = Message.find(params[:id])
+        # respond_to block
+        respond_to do |format|
+            format.html do
+                if message.update(params.message(:message).permit(:body, :title, :recipient_name,:sender_name, :recipient_id))
+                    # success message
+                    flash[:success] = 'Message updated successfully'
+                    # redirect to index
+                    redirect_to messages_url
+                else
+                    # error message
+                    flash.now[:error] = 'Error: Message could not be updated'
+                    # render edit
+                    render :edit, locals: { message: message }
+                end
+            end
+        end
+    end
+
+    def destroy
+        # load existing object again from URL param
+        message = Message.find(params[:id])
+        message.destroy
+        # respond_to block
+        respond_to do |format|
+            format.html do
+                # success message
+                flash[:success] = 'Message removed successfully'
+                # redirect to index
+                redirect_to messages_url
+            end
+        end
+    end
+end
---
+++ app/models/message.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id             :bigint           not null, primary key
+#  body           :text
+#  recipient_name :string
+#  sender_name    :string
+#  title          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  recipient_id   :integer
+#  user_id        :bigint
+#
+class Message < ApplicationRecord
+end
