64e58ac85f947537e79a89d619e6923c773fdfa1
Sohail Khalid <skhalid@memphis.edu>
2020-11-07 14:18:13 -0600

    add sender name on review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_204914) do
+ActiveRecord::Schema.define(version: 2020_11_07_194731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_204914) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "sender_name"
   end
 
   create_table "users", force: :cascade do |t|
